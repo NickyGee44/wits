0d00cf7f78d00ebaa23c9b694e95688106168ca3
Hyun Kim <hkim@live.com.au>
2023-11-16 06:50:52 +0400

    chore: upload addresses

--- contracts/src/scripts/set-live.ts
+++ contracts/src/scripts/set-live.ts
@@ -2,8 +2,12 @@ import { ethers, upgrades, run } from 'hardhat';
 
 async function main() {
   const test = [
-    '0x3B502B054715A8e0D8F657169615A88B2CCDD429',
-    '0x1B90a714A6609f57a5f78A846fcFC617B1f17237',
+    '0x2b561a6C8e8F9ac79467B616a7685AF35EBF0bda',
+    '0xa858bC58EEc4075f40C4b962C58b21856BF30C23',
+    '0xF23464360622b83d02e1523F11F50bFEA2a355A6',
+    '0xDfB943Fe445013690203298d6DDDE80F17662702',
+    '0x801aCC2065E3eaF4fCb81866DD05888D41219000',
+    '0xb8C75f107e6b0f68bbc480899ddc0645C8B93Ab6',
   ];
   const packets = await ethers.getContractAt(
     'Packets',
