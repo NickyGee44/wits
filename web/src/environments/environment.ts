2f2af367fcb3a19819c261f8896d58e39e7e5e7a
AayushCodes <aayushcodes@gmail.com>
2024-04-15 23:37:21 +0530

    migrations to arbitrum

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -17,8 +17,8 @@ export const environment = {
   mainnet: {
     gBabies: '0x0ff1f3a53f521d74af73fe7433abf802f91ddd4e' as const,
     quillAndInk: '0xd38123a8f534a7bbcefefa59e74f254a46818538' as const,
-    cards: '0x3F99Fd7772fa930E70F6560107424492482f3785' as const,
-    packets: '0xD783AbCeb12cdE361269fE7c0e3a695F08Be1830' as const,
+    cards: '0x0E32c3d657Ee2Efc03ed16B48e4e0C8B83C60780' as const,
+    packets: '0x470c8063388fd578eBC9556e6DC9F146A1EDBDFC' as const,
   },
   // mainnet: {
   //   gBabies: '0x0ff1f3a53f521d74af73fe7433abf802f91ddd4e' as const,
